ab5b466cc6c37b2f6acba8bba8f3ecf906b1f5ab
Suhaib Khan <suhaib96@gmail.com>
2020-04-17 19:18:30 +0530

    Old devices managed with different values folders

--- app/src/main/java/com/suhaibknx/webview/MainActivity.java
+++ app/src/main/java/com/suhaibknx/webview/MainActivity.java
@@ -1,10 +1,6 @@
 package com.suhaibknx.webview;
 
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,20 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // To get black status bar on devices < M (23)
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            Window window = MainActivity.this.getWindow();
-
-            // Set this flag
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-
-            // Clear this flag, as per android documentation
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-            // TODO this is yet to be tested
-            window.setStatusBarColor(Color.BLACK);
-        }
 
         webview = findViewById(R.id.webview);
 
