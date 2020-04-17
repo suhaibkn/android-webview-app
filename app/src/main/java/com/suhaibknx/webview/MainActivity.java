a43502be4d3a37e87cd9471ddd25b1ca73e6472c
Suhaib Khan <suhaib96@gmail.com>
2020-04-17 18:31:57 +0530

    First functional draft with INTERNET permission

--- app/src/main/java/com/suhaibknx/webview/MainActivity.java
+++ app/src/main/java/com/suhaibknx/webview/MainActivity.java
@@ -1,14 +1,24 @@
 package com.suhaibknx.webview;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    String loadURL = "https://www.google.com";
+    WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webview = findViewById(R.id.webview);
+
+        //Load a URL on WebView
+        webview.loadUrl(this.loadURL);
     }
+
 }
